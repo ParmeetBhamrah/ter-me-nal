d77d19d6fb2e7580e61d8a4c3f271d583edddd70
Parmeet <parmeetbhamrah@gmail.com>
2026-02-10 00:46:52 +0530

    Add auto-focus input functionality

--- src/components/TerminalInput.jsx
+++ src/components/TerminalInput.jsx
@@ -1,18 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const TerminalInput = ({ addCommand }) => {
     const [currentText, setCurrentText] = useState("")
-    
+    const inputRef = useRef(null)
+
+    useEffect(() => {
+        const focusInput = () => {
+            inputRef.current.focus()
+        }
+        window.addEventListener('click', focusInput)
+
+        return () => {
+            window.removeEventListener('click', focusInput)
+        }
+    }, [])
+
     const handleSubmit = (e) => {
         e.preventDefault();
         if (!currentText) return
         addCommand(currentText)
         setCurrentText("")
+        inputRef.current.focus()
     }
+
     return (
         <form onSubmit={handleSubmit}>
             <span className="mr-2 text-green-400 font-mono font-extrabold">parmeet@portfolio:~$</span>
             <input 
+            ref={inputRef}
                 type='text'
                 value={currentText}
                 onChange={e => setCurrentText(e.target.value)}
